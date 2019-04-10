43adf3e9f23d0fe14d2288e0591b2dbe8eb96dbd
LucasLimaAZ <40613140+LucasLimaAZ@users.noreply.github.com>
2019-04-09 21:31:58 -0300

    save

---
+++ back-end/app/controllers/PDF/ContratosClienteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers\PDF;
+
+use App\Models\Contrato;
+use Dompdf\Dompdf;
+
+class ContratosClienteController
+{
+
+    public function index()
+    {
+        $contrato = Contrato::get(["futuro", 1]);
+
+        setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+        date_default_timezone_set('America/Sao_Paulo');
+
+        header('Content-type: text/html; charset=UTF-8');
+        ob_start();
+
+        include_once "app/views/contratosFuturos.php";
+
+        $html = ob_get_contents();
+
+        ob_end_clean();
+
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream("codexworld", array("Attachment" => 0));
+    }
+}
