36223310b82f91e9c1fe48b1db68b99782ab7e6b
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2019-06-18 10:44:14 -0300

    imediato nao pede mais data

--- sistema/contratos.php
+++ sistema/contratos.php
@@ -515,7 +515,7 @@
 			days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
 			daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
 			daysMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
-			months: ["Janeirp", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julio", "Agosto", "Setmebro", "Outubro", "Novembro", "Dezembro"],
+			months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
 			monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
 			today: "Hoje",
 			clear: "Limpar",
